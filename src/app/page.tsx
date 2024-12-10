cd774fd59b7f77ff49467cdfc5659b99e07bcc9a
Aleksey <avzag@yandex.ru>
2024-12-10 15:34:14 +0300

    ошибка tailwind.config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import type { Metadata } from "next"
 import RecipeCard from "@/components/recipes/RecipeCard"
 import type { Recipe } from "@prisma/client"
+import HeroSection from "@/components/HeroSection"
 
 export const metadata: Metadata = {
   title: "Книга рецептов",
@@ -21,20 +22,40 @@ export default async function Home(): Promise<React.JSX.Element> {
   const dailyRecipe = id ? await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recipes/${id}`).then((res) => res.json()) : null
 
   const recipe: Recipe = await fetchRecipeById()
+  
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold">Добро пожаловать на страницу рецептов</h1>
-      <p className="mx-4 text-lg mt-4">Делитесь любимыми рецептами и открывайте для себя новые!</p>
-
+      {/* <h1 className="text-4xl font-bold">Добро пожаловать на страницу рецептов</h1>
+      <p className="mx-4 text-lg mt-4">Делитесь любимыми рецептами и открывайте для себя новые!</p> */}
+      <HeroSection
+        headline="Откройте для себя мир кулинарных шедевров"
+        subheadline="Добро пожаловать в книгу рецептов, где каждый рецепт – это маленький праздник! Найдите вдохновение для создания удивительных блюд, которые порадуют вас и ваших близких."
+        primaryBtnText="Узнать больше"
+        primaryBtnLink="/about"
+        secondaryBtnText="Смотреть видео"
+        secondaryBtnLink="/watch-demo"
+        alertText="Попробуйте наши самые популярные рецепты!"
+        alertLink="/recipes"
+        alertBadge="Новинка"
+        popularText="Популярно"
+        images={[
+          { href: "/recipes/1", src: "/images/carbonara.jpg", alt: "Carbonara", width: 80, height: 40 },
+          { href: "/recipes", src: "/images/omelette.jpg", alt: "Omelette", width: 100, height: 50 },
+          { href: "/recipes/2", src: "/images/pizza.jpg", alt: "Pizza", width: 100, height: 50 },
+          { href: "/recipes/1", src: "/images/carbonara.jpg", alt: "Carbonara", width: 80, height: 40 },
+          { href: "/recipes/3", src: "/images/salad.jpg", alt: "Salad", width: 90, height: 45 },
+        ]}
+      />
+      
       <nav className="mt-6">
-        <Link
+        {/* <Link
           className="text-blue-500 hover:underline"
           href="/recipes"
         >
           Посмотреть рецепты
-        </Link>
+        </Link> */}
       </nav>
-      {dailyRecipe && (
+      {/* {dailyRecipe && (
         <>
           <h2 className="text-2xl mt-12 mb-4 font-semi">Рекомендуемый рецепт</h2>
           <div className="w-fit">
@@ -47,7 +68,8 @@ export default async function Home(): Promise<React.JSX.Element> {
             />
           </div>
         </>
-      )}
+      )} */}
+      
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -14,6 +15,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [flowbite.plugin(),],
+}
+export default config
